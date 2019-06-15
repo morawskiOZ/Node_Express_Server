43ed3839416c67015d3a9fd43c811905cf563056
Piotr Morawski <piotrmorawski90@gmail.com>
2019-06-15 15:42:52 +0200

    merge master

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const nodemailer_1 = __importDefault(require("nodemailer"));
 const cors_1 = __importDefault(require("cors"));
+const src_1 = __importDefault(require("./src/"));
+const errorHandlers_1 = __importDefault(require("./handlers/errorHandlers"));
 const app = express_1.default();
 const port = 4444;
 app.use(body_parser_1.default.json());
@@ -16,49 +17,8 @@ app.listen(port, () => {
     console.log("We are live on port 4444");
     console.log(process.env.MAIL_HOST);
 });
-app.get("/", (req, res) => {
-    console.log("dziala");
-    res.send("Welcome to my api");
-});
-app.post("/api/v1", (req, res) => {
-    const data = req.body;
-    console.log(data);
-    console.log(process.env.MAIL_HOST);
-    const smtpTransport = nodemailer_1.default.createTransport({
-        host: process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        secure: false,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
-        }
-    });
-    smtpTransport.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-        }
-        else {
-            console.log("Server is ready to take our messages");
-        }
-    });
-    var mailOptions = {
-        from: data.email,
-        to: "ENTER_YOUR_EMAIL",
-        subject: "ENTER_YOUR_SUBJECT",
-        html: `<p>${data.name}</p>
-          <p>${data.email}</p>
-          <p>${data.message}</p>`
-    };
-    smtpTransport.sendMail(mailOptions, (error, response) => {
-        if (error) {
-            res.send(error);
-            console.log("Msg fail");
-        }
-        else {
-            res.send("Success");
-            console.log("Msg succ");
-        }
-        smtpTransport.close();
-    });
-});
+// I am using routes to allow for scaling of this boilerplate!
+app.use('/', src_1.default);
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers_1.default.notFound);
 //# sourceMappingURL=index.js.map
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,9 @@ import express from "express";
 import bodyParser from "body-parser";
 import nodemailer from "nodemailer";
 import cors from "cors";
+import routes from './src/'
+import errorHandlers from './handlers/errorHandlers'
+
 
 const app = express()
 
@@ -17,51 +20,8 @@ app.listen(port, () => {
   console.log(process.env.MAIL_HOST)
 })
 
-app.get("/", (req, res) => {
-  console.log("dziala")
-  res.send("Welcome to my api")
-})
+// I am using routes to allow for scaling of this boilerplate!
+app.use('/', routes);
 
-app.post("/api/v1", (req, res) => {
-  const data = req.body
-  console.log(data)
-  console.log(process.env.MAIL_HOST)
-  
-  const smtpTransport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
-    }
-  }as any)
-
-  smtpTransport.verify(function(error, success) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Server is ready to take our messages");
-    }
-  });
-
-  var mailOptions = {
-    from: data.email,
-    to: "ENTER_YOUR_EMAIL",
-    subject: "ENTER_YOUR_SUBJECT",
-    html: `<p>${data.name}</p>
-          <p>${data.email}</p>
-          <p>${data.message}</p>`
-  }
-
-  smtpTransport.sendMail(mailOptions, (error, response) => {
-    if (error) {
-      res.send(error)
-      console.log("Msg fail")
-    } else {
-      res.send("Success")
-      console.log("Msg succ")
-    }
-    smtpTransport.close()
-  })
-})
+// If that above routes didnt work, we 404 them and forward to error handler
+app.use(errorHandlers.notFound);
