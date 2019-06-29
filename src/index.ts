10bab707c282aa6d5fc23c029238e8f0dd51a8e7
Piotr Morawski <piotrmorawski90@gmail.com>
2019-06-29 16:07:59 +0200

    server side rendering works

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import bodyParser from "body-parser"
 import cors from "cors"
-import express from "express"
 import routes from "./routes/index"
-const errorHandlers = require('./handlers/errorHandlers');
+import bodyParser from "body-parser"
+import path from "path"
+const express = require("express")
+const errorHandlers = require("./handlers/errorHandlers")
 
 const app = express()
 
@@ -16,7 +17,9 @@ app.use(cors())
 app.listen(port, () => {
   console.log("We are live on port 4444")
 })
-
+app.use(
+  express.static(path.join(__dirname, "../../piotr_morawski_portfolio/build"))
+)
 // I am using routes to allow for scaling of this boilerplate!
 app.use("/", routes)
 
